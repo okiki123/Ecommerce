e3772be93f67ee18631dd01acc8a4bb5d505ca8f
okiki123 <okikimalomo17@gmail.com>
2022-01-26 23:49:51 +0100

    Updated Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { Search } from '@mui/icons-material';
+import React from 'react';
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 60px;
+`
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`
+const Left = styled.div`
+  flex: 1;
+  diaplay: flex;
+  align-items: center;
+`
+const Center = styled.div`
+flex: 1;
+`
+const Right = styled.div`
+flex: 1;
+`
+const Language = styled.span`
+font-size: 14px;
+cursor: pointer;
+`
+const SearchContainer = styled.div`
+border: 1px solid lightgray;
+`
+const Navbar = () => {
+  return (
+  <Container>
+    <Wrapper>
+      <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+              <Search />
+          </SearchContainer>
+      </Left>
+      <Center>Center</Center>
+      <Right>Right</Right>
+    </Wrapper>
+  </Container>
+  );
+};
+
+export default Navbar;
